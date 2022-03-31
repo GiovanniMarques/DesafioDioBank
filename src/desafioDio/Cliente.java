94d0303c602683e06a4f845868cad630f97ea341
giova <giovanni_marques11@hotmail.com>
2022-03-31 15:44:42 -0300

    DesafioDioBank

---
+++ src/desafioDio/Cliente.java
@@ -0,0 +1,27 @@
+package desafioDio;
+
+public class Cliente {
+	
+	public String nome;
+
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public void sacar() {
+		
+	}
+
+	public void depositar() {
+		
+	}
+
+	public void transferir() {
+	
+}
+}
---
+++ src/desafioDio/ContaCorrente.java
@@ -0,0 +1,16 @@
+package desafioDio;
+
+public class ContaCorrente extends Conta {
+
+	@Override
+	public void imprimirExtrato() {
+		super.imprimirInfosComuns();
+				
+		
+	}
+
+
+	
+	
+}
+
